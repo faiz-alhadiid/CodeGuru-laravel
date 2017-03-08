46ad3f6f6898c2479542bb8a15cd11c207cd8e26
tituschristian <tituschristian98@gmail.com>
2017-03-08 20:43:44 +0700

    footer login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -65,4 +65,13 @@
         </div>
     </div>
 </div>
+<div class="footer_bg"><!-- start footer -->
+    <div class="container">
+        <div class="row  footer">
+            <div class="copy text-center">
+                <p class="link"><span>&#169; All rights reserved | Design by CODEGURU</span></p>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
